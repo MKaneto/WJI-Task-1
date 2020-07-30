a9f5ce61595785ab99ad71921062b1fdb33d41f5
Mkaneto <kanny.mbachu@gmail.com>
2020-07-30 17:34:21 +0100

    intro

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+// vue.config.js
+module.exports = {
+        publicPath: '/WJI-Task-1/'
+    }
+
+// module.exports = {
+//     publicPath: process.env.NODE_ENV === 'production'
+//         ? '/WJI-Task-1/'
+//         : '/'
+//     }
